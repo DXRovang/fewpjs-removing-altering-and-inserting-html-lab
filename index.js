cfe9c3eb2a52e6391f09d844509875de25db595e
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-13 19:26:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById('main').remove();
+// main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+newHeader.innerHTML = "Xander is the champion"
